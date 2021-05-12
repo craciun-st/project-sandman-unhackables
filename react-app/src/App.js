cdc90497adea3d8322baa777b54d39ab9549e78a
DanielCIstrate <daniel.istr8@gmail.com>
2021-05-12 14:03:16 +0300

    ADD functionalities: add new task and delete task from list -- react/client only

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.css'
+import {useState} from 'react';
 
-const taskData = [
+var taskData = [
   {
       "id": 1411077732,
       "name": "Assignments"
@@ -23,7 +24,27 @@ const userId = 1;
 const userName = "Developer";
 
 
+
 function App() {
+
+  const[taskName, setTaskName] = useState('');
+  const[taskList, setTaskList] = useState(taskData);
+
+
+  function addTask() {
+    let newList = [...taskList];
+    newList.push({"name": taskName});
+    setTaskList(newList);
+  }
+
+  function deleteTask(index) {
+    var duplicateArray = [...taskList];
+    duplicateArray.splice(index,1);
+    setTaskList(duplicateArray);
+  }
+  
+
+
   return (
     <div className="App">
       <div className="login-container">
@@ -31,22 +52,33 @@ function App() {
           {userName}
           </span>
       </div>
-      <div>
-        <table className="table table-success table-striped">
-          <tbody>
-            {taskData.map(
-              (task, index) => (
-                <tr key={index}>
-                  <td>{index+1}</td>
-                  <td>{task.name}</td>
-                </tr>
-              )
-            )}
-            </tbody>
-        </table>
+      <div className="row justify-content-center">
+        <div className="col-md-5">
+          
+          <input type="text" placeholder="Enter task..." className="form-control" 
+          value={taskName} onChange={(e) => {setTaskName(e.target.value)}}
+          />
+          <button className="btn btn-primary" onClick={addTask}>ADD</button>
+
+          <table className="table table-success table-striped">
+            <tbody>
+              {taskList.map(
+                (task, index) => (
+                  <tr key={index}>
+                    <td>{index+1}</td>
+                    <td>{task.name}</td>
+                    <td><i class="far fa-trash-alt" onClick={() => deleteTask(index)}></i></td>
+                  </tr>
+                )
+              )}
+              </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
+
+
 }
 
 export default App;
