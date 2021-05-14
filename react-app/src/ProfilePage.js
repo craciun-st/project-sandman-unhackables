762e850a4f9289a1f1f2d090f0d6f53b666cedf7
craciun-st <67415094+craciun-st@users.noreply.github.com>
2021-05-14 08:49:11 +0300

    Update ProfilePage to include user avatar and a chart

--- react-app/src/ProfilePage.js
+++ react-app/src/ProfilePage.js
@@ -1,11 +1,18 @@
 import './App.css';
+import './ProfilePage.css'
 import bootstrap from 'bootstrap/dist/css/bootstrap.min.css'
 import { React, useState } from "react";
+import ChartistLineGraph from "./chart";
 
 function ProfilePage() {
+    const userName = 'Developer'    
     return (
         <div className="ProfilePage">
-            <h1>This is the profile page</h1>
+            <h2 className="nameContainer">Hello, {userName}</h2>
+            <img src="https://picsum.photos/300/300" className="profilePicDim"></img>
+            <div className="chart-container">
+                <ChartistLineGraph></ChartistLineGraph>
+            </div>
         </div>
     )
 }
