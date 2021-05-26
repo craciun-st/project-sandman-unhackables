e276ce55ec1c7ba03f2311a8c25ac2effc134283
craciun-st <67415094+craciun-st@users.noreply.github.com>
2021-05-26 09:12:28 +0300

    Add more initial objects to persist in DatabaseInit

--- demo/src/main/java/com/codecool/demo/config/DatabaseInit.java
+++ demo/src/main/java/com/codecool/demo/config/DatabaseInit.java
@@ -1,5 +1,6 @@
 package com.codecool.demo.config;
 
+import com.codecool.demo.dao.MockUserSupplier;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -10,6 +11,7 @@ import com.codecool.demo.model.Task;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.transaction.Transactional;
+import java.util.*;
 
 @Component
 public class DatabaseInit implements ApplicationRunner {
@@ -25,13 +27,69 @@ public class DatabaseInit implements ApplicationRunner {
     @Override
     @Transactional
     public void run(ApplicationArguments args) throws Exception {
-
+        Random randomizer = new Random();
         User admin = new User(null, "Admin");
+        User john = new User(null, "John");
+        User jane = new User(null, "Jane");
+        User ionel = new User(null, "Ionel");
+        User giovanna = new User(null, "Giovanna");
+        List<User> initialUsers = new LinkedList<>();
+        initialUsers.add(admin);
+        initialUsers.add(john);
+        initialUsers.add(jane);
+        initialUsers.add(ionel);
+        initialUsers.add(giovanna);
+
+
         Task testTask = new Task(null,"Finish homework");
-        admin.addTask(testTask);    // when adding a task, its userOwner is also set within this method
+        admin.addTask(testTask); // when adding a task, its userOwner is also set within this method
+        Task task1 = new Task(null, "Study");
+        Task task2 = new Task(null, "Jog in the park");
+        Task task3 = new Task(null, "Buy groceries");
+        Task task4 = new Task(null, "Another task");
+        Task task5 = new Task(null, "Yet Another Task (YAT)");
+        Task task6 = new Task(null, "Evil mastermind scheme");
+        Task task7 = new Task(null, "Get some sleep");
+
+        List<Task> initialTasks = new ArrayList<>();
+        initialTasks.add(testTask);
+        initialTasks.add(task1);
+        initialTasks.add(task2);
+        initialTasks.add(task3);
+        initialTasks.add(task4);
+        initialTasks.add(task5);
+        initialTasks.add(task6);
+        initialTasks.add(task7);
+        for (int j = 0; j < 7; j++) {
+            initialTasks.add(MockUserSupplier.getRandomTaskWithNullId(randomizer.nextLong()));
+        }
+
+        for (int i = 0; i < 4; i++) {
+
+            john.addTask(initialTasks.get(i+1));
+
+        }
+        for (int i = 0; i<3; i++) {
+
+            jane.addTask(initialTasks.get(i+5));
+
+        }
+
+        for (int i = 0; i < 4; i++) {
+            ionel.addTask(initialTasks.get(i+8));
+        }
+
+        for (int i = 0; i < 3; i++) {
+            giovanna.addTask(initialTasks.get(i+12));
+        }
+
         try {
-            entityManager.persist(admin);
-            entityManager.persist(testTask);
+            for (User user : initialUsers) {
+                entityManager.persist(user);
+            }
+            for (Task task : initialTasks) {
+                entityManager.persist(task);
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
--- demo/src/main/java/com/codecool/demo/dao/HibernateEntityManagerInit.java
+++
@@ -1,5 +0,0 @@
-package com.codecool.demo.dao;
-
-public class HibernateEntityManagerInit {
-
-}
