2a05afb19e10fa63eb72db5db08b17414c5881e7
craciun-st <67415094+craciun-st@users.noreply.github.com>
2021-05-27 13:17:59 +0300

    Allow CrossOrigin requests in TasksController

--- demo/src/main/java/com/codecool/demo/controller/TasksController.java
+++ demo/src/main/java/com/codecool/demo/controller/TasksController.java
@@ -16,6 +16,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin(origins = "*")     // not very safe. configure this to trusted http domains!
 @RestController
 public class TasksController {
 
