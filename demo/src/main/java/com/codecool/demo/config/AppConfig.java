6f436f7d598b73d092faf6d3ae9211abf0f4a168
DanielCIstrate <daniel.istr8@gmail.com>
2021-05-27 11:17:31 +0300

    Patched annotations in AppConfig to set repo package

--- demo/src/main/java/com/codecool/demo/config/AppConfig.java
+++ demo/src/main/java/com/codecool/demo/config/AppConfig.java
@@ -6,13 +6,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+@EnableJpaRepositories("com.codecool.demo.repositories")
+@EnableTransactionManagement
 @Configuration
 @ComponentScan
-@EnableJpaRepositories
 public class AppConfig {
 
 //    @Bean
