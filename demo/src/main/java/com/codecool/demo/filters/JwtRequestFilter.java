b0e19b74fb2b8b5c8a9ad60c8e8b12f3eb6b117d
craciun-st <67415094+craciun-st@users.noreply.github.com>
2021-06-09 15:54:58 +0300

    Add a filter for HTTP requests to preprocess their headers for JWT
    Just the skeleton at the moment, no logic for validation

---
+++ demo/src/main/java/com/codecool/demo/filters/JwtRequestFilter.java
@@ -0,0 +1,21 @@
+package com.codecool.demo.filters;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component  // needed! filter is autowired in WebSecurityConfig class
+public class JwtRequestFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        // JWT-based authorization happens here, before the request is sent to the controller
+
+        filterChain.doFilter(request, response);
+    }
+}
