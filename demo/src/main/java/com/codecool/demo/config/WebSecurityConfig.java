064b7d8eea7d73f1db8059e7414b2a736c91fa36
craciun-st <67415094+craciun-st@users.noreply.github.com>
2021-06-10 07:59:19 +0300

    Add a DaoAuthenticationProvider
    A more standardized way to handle authentication via passwords
    stored in a database.
    It depends on both OurUserDetailsService and PasswordEncoder

--- demo/src/main/java/com/codecool/demo/config/WebSecurityConfig.java
+++ demo/src/main/java/com/codecool/demo/config/WebSecurityConfig.java
@@ -2,12 +2,15 @@ package com.codecool.demo.config;
 
 import com.codecool.demo.filters.JwtRequestFilter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -15,6 +18,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.security.SecureRandom;
 
 @Configuration
 @EnableWebSecurity
@@ -62,6 +67,22 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                                                                         // as the salt
 
 //        return NoOpPasswordEncoder.getInstance();     // to test against errors due to wrong wiring of encoding
+    }
+
+
+    @Bean
+    public AuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider =
+                new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder());
+        provider.setUserDetailsService(this.ourUserDetailsService);
+        return provider;
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring().requestMatchers(PathRequest.toH2Console());
     }
 
     @Override
