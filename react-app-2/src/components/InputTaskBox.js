659b2dffca2f2809512e1d48d272bb876420cfa1
DanielCIstrate <daniel.istr8@gmail.com>
2021-06-23 15:45:37 +0300

    Add InputTaskBox for new task text

---
+++ react-app-2/src/components/InputTaskBox.js
@@ -0,0 +1,16 @@
+import './InputTaskBox.css'
+import React, { Component } from 'react'
+
+
+export class InputTaskBox extends Component {
+    render() {
+        return (
+            <div className="input-task-box">
+                <input type="text"></input>
+                <input type="text"></input>
+            </div>
+        )
+    }
+}
+
+export default InputTaskBox
