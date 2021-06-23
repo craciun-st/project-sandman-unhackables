c158b928f10fdc84b69640c974b29ab5c323a991
DanielCIstrate <daniel.istr8@gmail.com>
2021-06-23 15:41:12 +0300

    AppPageBody is a 2x2 CSS grid

---
+++ react-app-2/src/components/AppPageBody.js
@@ -0,0 +1,19 @@
+import './AppPageBody.css'
+import React, { Component } from 'react'
+import TaskContainer from './TaskContainer'
+import Box2 from './Box2'
+import Box3 from './Box3'
+import Box4 from './Box4'
+
+export default class AppPageBody extends Component {
+    render() {
+        return (
+            <div className="AppPageBody">
+                <Box2/>
+                <Box3/>
+                <Box4/>
+                <TaskContainer/>
+            </div>
+        )
+    }
+}
