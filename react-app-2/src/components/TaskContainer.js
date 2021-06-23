f941c869810123c50665bf5b8d0a44f5b9616887
DanielCIstrate <daniel.istr8@gmail.com>
2021-06-23 15:43:56 +0300

    Add TaskContainer for old task operations

---
+++ react-app-2/src/components/TaskContainer.js
@@ -0,0 +1,15 @@
+import './TaskContainer.css'
+import React, { Component } from 'react'
+import InputTaskBox from './InputTaskBox'
+import TaskTable from './TaskTable'
+
+export default class TaskContainer extends Component {
+    render() {
+        return (
+            <div className="TaskContainer">
+                <InputTaskBox/>
+                <TaskTable/>            
+            </div>
+        )
+    }
+}
