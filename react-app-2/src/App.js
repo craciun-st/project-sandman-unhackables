490dfb58a86c9c044b097ebb0f43ae4d9c20aa90
DanielCIstrate <daniel.istr8@gmail.com>
2021-06-23 15:39:13 +0300

    Move boxes to AppPageBody class

--- react-app-2/src/App.js
+++ react-app-2/src/App.js
@@ -1,21 +1,18 @@
 import './App.css';
 import Navbar from './components/Navbar';
-import TaskTable from './components/TaskTable';
-import Box2 from './components/Box2';
-import Box3 from './components/Box3';
-import Box4 from './components/Box4';
+import AppPageBody from './components/AppPageBody';
+
 
 function App() {
   return (
     <div className="App">
       <div className="container main">
+    
         <Navbar/>
-        <TaskTable/>
-        <Box2/>
-        <Box3/>
-        <Box4/>
-        
+
+        <AppPageBody/>
       </div>
+
       <div className="container side-menu">
         This is the side menu
       </div>
