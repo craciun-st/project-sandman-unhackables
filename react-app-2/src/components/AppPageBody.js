8877650bdf8b68785a71427e5c48e9edd6e693d6
craciun-st <67415094+craciun-st@users.noreply.github.com>
2021-06-24 11:56:37 +0300

    Make AppPageBody handle the Task-related state
    Since we manage state through 'useState', we made AppPageBody a function
    component (and not a class!)
    
    Props are passed accordingly to the child InputTaskBox.
    (note that the mapped functions don't exist yet!)

--- react-app-2/src/components/AppPageBody.js
+++ react-app-2/src/components/AppPageBody.js
@@ -1,16 +1,30 @@
 import './AppPageBody.css'
-import React, { Component } from 'react'
+import React, { Component, useState, useEffect } from 'react'
 import TaskContainer from './TaskContainer'
 import Box2 from './Box2'
 import Box3 from './Box3'
 import Box4 from './Box4'
+import InputTaskBox from './InputTaskBox'
+import { updateTaskName, updateTaskCategory, addTask } from '../util/taskStateFunctions'
 
-export default class AppPageBody extends Component {
-    render() {
+export default function AppPageBody()  {
+
+        const [taskName, setTaskName] = useState("");
+        const [taskList, setTaskList] = useState(taskData);
+        const [canGetTaskList, setCanGetTaskList] = useState(false);
+        const [taskCategory, setTaskCategory] = useState("");
+        const [taskImportance, setTaskImportance] = useState("3");
+
         return (
             <div className="AppPageBody">
                 <Box2/>
                 <InputTaskBox 
+                    taskName={taskName} 
+                    updateTaskName={mappedUpdateTaskName}
+                    taskCategory={taskCategory}
+                    updateTaskCategory={mappedUpdateTaskCategory} 
+                    defaultCategories={defaultCategories}
+                    onAddTaskClick={mappedAddTask}                    
                 />
                 <Box4/>
                 <TaskContainer/>
