4aec6a658617b0709878899be45b8bd09ff8f05a
DanielCIstrate <daniel.istr8@gmail.com>
2021-06-24 19:18:54 +0300

    Add Navbar logo image
    Hide-show for side-menu property on click

--- react-app-2/src/components/Navbar.js
+++ react-app-2/src/components/Navbar.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react'
+import './Navbar.css'
+import logo from '../util/resources/image.png'
 
 export default class Navbar extends Component {
     render() {
+        function hideSideBar() {
+            var sideBar = document.getElementById("side-menu");
+            if (sideBar.style.display==="none") {
+                sideBar.style.display = "block"
+            } else {
+                sideBar.style.display = "none"
+            };
+        }
+
+
         return (
             <div className="Navbar">
-                This is classname: Navbar containing nav links
+                    <div className="logo-image" onClick={hideSideBar}>
+                        <img src={logo}
+                            alt="Empty login face"
+                            width="46"
+                            height="46"/>
+                    </div>
             </div>
         )
     }
