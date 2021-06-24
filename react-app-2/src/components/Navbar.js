845593cd89a7662a9829824166e50ab785542796
DanielCIstrate <daniel.istr8@gmail.com>
2021-06-24 20:45:30 +0300

    Update navbar to have a LogoImage

--- react-app-2/src/components/Navbar.js
+++ react-app-2/src/components/Navbar.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react'
 import './Navbar.css'
 import LogoImage from '../util/resources/LogoImage';
-import logo from '../util/resources/image.png'
+
 
 export default class Navbar extends Component {
     render() {
+        function hideSideBar() {
+            var sideBar = document.getElementById("side-menu");
+            if (sideBar.style.display==="none") {
+                sideBar.style.display = "flex"
+            } else {
+                sideBar.style.display = "none"
+            };
+        }
 
 
         return (
             <div className="Navbar">
 
                 <LogoImage
-                    logo={logo}/>
+                    onClickFunction={hideSideBar}
+                    logo={this.props.logo}
+                    imageWidth="30vw"/>
             </div>
         )
     }
