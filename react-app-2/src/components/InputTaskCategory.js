4ea22654d24e0c2f3a603d7c24ac96e0883aa2e0
craciun-st <67415094+craciun-st@users.noreply.github.com>
2021-06-24 11:41:44 +0300

    Add component for category input box
    Similar to taskName.
    It also gets a defaultCategories prop to set its <datalist />.

---
+++ react-app-2/src/components/InputTaskCategory.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+
+export default class InputTaskCategory extends Component {
+    render() {
+        return (
+            <div>
+                <input
+                            list="default-categories"
+                            id="category-choice"
+                            name="category-choice"
+                            className=""
+                            value={this.props.taskCategory}
+                            onChange={this.props.updateTaskCategory}
+                            placeholder="Enter task category..."
+                />
+                        
+                <datalist id="default-categories">
+                    {this.props.defaultCategories.map(
+                        (category) => (
+                            <option value={category} />
+                        )
+                    )}
+                </datalist>
+            </div>          
+        )
+    }
+}
