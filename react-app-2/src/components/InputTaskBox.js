56b531d010b72a7a4e7cb70d614a7fe54ee64fde
craciun-st <67415094+craciun-st@users.noreply.github.com>
2021-06-24 11:48:44 +0300

    Make InputTaskBox hold the 2 inputs and the button
    Also handle their props.
    Note that, in turn, these values will be obtained from the parent of
    InputTaskBox

--- react-app-2/src/components/InputTaskBox.js
+++ react-app-2/src/components/InputTaskBox.js
@@ -1,13 +1,25 @@
 import './InputTaskBox.css'
 import React, { Component } from 'react'
+import InputTaskName from './InputTaskName'
+import AddTaskButton from './AddTaskButton'
+import InputTaskCategory from './InputTaskCategory'
 
 
 export class InputTaskBox extends Component {
     render() {
         return (
             <div className="input-task-box">
-                <input type="text"></input>
-                <input type="text"></input>
+                <div className="itb-wide-container">
+                    <InputTaskName taskName={this.props.taskName} updateTaskName={this.props.updateTaskName}/>
+                    <InputTaskCategory 
+                        taskCategory={this.props.taskCategory} 
+                        updateTaskCategory={this.props.updateTaskCategory}
+                        defaultCategories={this.props.defaultCategories}
+                    />
+                </div>
+                <div className="itb-side-container">
+                    <AddTaskButton addTask={this.props.onAddTaskClick}/>
+                </div>
             </div>
         )
     }
