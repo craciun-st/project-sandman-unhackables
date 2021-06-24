484e1d75055064b33b1ac2bc05c77e1b0833bfe0
DanielCIstrate <daniel.istr8@gmail.com>
2021-06-24 19:20:26 +0300

    Edit class not to use container  bc boostrap conflict

--- react-app-2/src/App.js
+++ react-app-2/src/App.js
@@ -4,16 +4,18 @@ import AppPageBody from './components/AppPageBody';
 
 
 function App() {
+
+  
   return (
     <div className="App">
-      <div className="container main">
+      <div className="main-part">
     
         <Navbar/>
 
         <AppPageBody/>
       </div>
 
-      <div className="container side-menu">
+      <div className="side-menu" id="side-menu">
         This is the side menu
       </div>
     </div>
