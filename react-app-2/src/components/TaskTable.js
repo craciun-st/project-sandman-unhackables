43ab04753f708c8816a4f355b7cda45b7b2f8fce
DanielCIstrate <daniel.istr8@gmail.com>
2021-06-24 15:08:49 +0300

    Add new component for TaskTable
    Uses the old code from the previous app version
    
    Some functions were converted to props.

--- react-app-2/src/components/TaskTable.js
+++ react-app-2/src/components/TaskTable.js
@@ -1,12 +1,45 @@
 import './TaskTable.css'
 import React, { Component } from 'react'
+import { colorClassForCategory } from '../util/cssHelperFunctions'
+import bootstrap from "bootstrap/dist/css/bootstrap.min.css";
 
 export default class TaskTable extends Component {
     render() {
         return (
-            <div className="TaskTable">
-                TaskTable
-            </div>
+            <table className="table table-success table-striped">
+                <tbody>
+                    {this.props.taskList.map((task, index) => (
+                    <tr key={index}>
+                        <td>
+                        <input
+                            type="checkbox"
+                            id={`check-${index}`}
+                            onChange={this.props.handleCheckBoxClick}
+                            checked={task.done}
+                        ></input>
+                        </td>
+                        <td>{index + 1}</td>
+                        <td>
+                        <div
+                            className={`category-text ${colorClassForCategory(
+                            task.category
+                            )}`}
+                        >
+                            {task.category}
+                        </div>
+                        </td>
+                        <td>{task.name}</td>
+                        <td>{task.importance}</td>
+                        <td>
+                            <i
+                                className="far fa-trash-alt red-color-text"
+                                onClick={() => this.props.deleteTask(index)}
+                            ></i>
+                        </td>
+                    </tr>
+                    ))}
+                </tbody>
+            </table>
         )
     }
 }
