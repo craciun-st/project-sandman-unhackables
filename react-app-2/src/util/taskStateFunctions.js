1f2e1b33a1b9af3c46f25f5bd0752a757544aad4
craciun-st <67415094+craciun-st@users.noreply.github.com>
2021-06-24 11:52:00 +0300

    Port Task state handling functions from old code
    It is in a separate JS file in 'util' package
    Since we do not have access to the state variables and their setters,
    we make them use more parameters to specify the said variables
    (and their setters).

---
+++ react-app-2/src/util/taskStateFunctions.js
@@ -0,0 +1,51 @@
+export function updateTaskName(event, setterFunc) {
+    setterFunc(event.target.value);            
+}
+
+export function updateTaskCategory(event, setterFunc) {
+    setterFunc(event.target.value);
+}
+
+export function createNewTask(taskName, taskCategory, importance = null) {
+    let nameString;
+    let categoryString;
+    let importanceInt;
+    if (!(typeof taskName === "string")) {
+      nameString = "";
+    } else {
+      nameString = taskName.slice();
+    }
+
+    if (!(typeof taskCategory === "string")) {
+      categoryString = "";
+    } else {
+      categoryString = taskCategory.slice();
+    }
+
+    if (!(typeof importance === "string")) {
+      if (!(typeof importance === "number")) {
+        importanceInt = 3;
+      } else {
+        importanceInt = importance;
+      }
+    } else {
+      importanceInt = parseInt(importance);
+    }
+
+    return {
+      name: nameString,
+      importance: importanceInt,
+      category: categoryString,
+      done: false,
+    };
+  }
+
+export function addTask(taskList, taskMap, taskSetterMap) {
+    let newList = [...taskList];
+    newList.push(createNewTask(taskMap.name, taskMap.category, taskMap.importance));
+    taskSetterMap['setTaskList'](newList);
+    taskSetterMap['setTaskName']("");
+    taskSetterMap['setTaskCategory']("");
+    taskSetterMap['setTaskImportance']("3");
+    console.log(newList);
+}
