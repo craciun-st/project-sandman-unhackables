f9fc8836057f228703b28bef96f2cb02da77e0da
DanielCIstrate <daniel.istr8@gmail.com>
2021-06-24 20:49:07 +0300

    Edit remove className prop not needed anymore

--- react-app-2/src/App.js
+++ react-app-2/src/App.js
@@ -1,22 +1,32 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import AppPageBody from './components/AppPageBody';
+import LogoImage from './util/resources/LogoImage';
+import LoginButton from './components/authentication/LoginButton';
+import logo from './util/resources/image.png'
 
 
 function App() {
+
 
   
   return (
     <div className="App">
       <div className="main-part">
     
-        <Navbar/>
+        <Navbar
+        logo={logo}/>
 
         <AppPageBody/>
       </div>
 
       <div className="side-menu" id="side-menu">
         This is the side menu
+        <LogoImage
+          logo={logo}
+          imageWidth="80%"/>
+        <LoginButton/>
+
       </div>
     </div>
   );
