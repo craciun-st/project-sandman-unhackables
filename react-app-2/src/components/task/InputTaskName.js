6cbacfcd6fd03ff456bb15e5c23960387a86873a
craciun-st <67415094+craciun-st@users.noreply.github.com>
2021-06-25 04:35:04 +0300

    Make InputTaskName.js return a 'div input'
    This is to be consistent with InputTaskCategory, which does
    the same

--- react-app-2/src/components/task/InputTaskName.js
+++ react-app-2/src/components/task/InputTaskName.js
@@ -3,13 +3,15 @@ import React, { Component } from 'react'
 export default class InputTaskName extends Component {
     render() {
         return (
-            <input
-                        type="text"
-                        placeholder="Enter task..."
-                        className=""
-                        value={this.props.taskName}
-                        onChange={this.props.updateTaskName}
-            />
+            <div>
+                <input
+                            type="text"
+                            placeholder="Enter task..."
+                            className=""
+                            value={this.props.taskName}
+                            onChange={this.props.updateTaskName}
+                />
+            </div>
         )
     }
 }
