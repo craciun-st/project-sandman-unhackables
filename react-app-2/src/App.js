bd6b177e1b47f90f8a1bf460ff045a25ff73491f
craciun-st <67415094+craciun-st@users.noreply.github.com>
2021-07-12 10:31:59 +0300

    Add a router and loginStatus functionality in App.js

--- react-app-2/src/App.js
+++ react-app-2/src/App.js
@@ -1,38 +1,59 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {useState} from "react";
 import Navbar from './components/Navbar';
 import AppPageBody from './components/AppPageBody';
 import LogoImage from './components/LogoImage';
 import LoginButton from './components/authentication/LoginButton';
 import logo from './util/resources/image.png'
 import LogoutButton from "./components/authentication/LogoutButton";
+import LoginPage from "./components/authentication/LoginPage";
 
 
-const loginStatus = false;
+
 
 function App() {
 
+  const [loginStatus, setLoginStatus] = useState(true);
 
+  function handleLoginButtonClick(event) {
+      window.location.href = "/login"
+  }
   
   return (
-    <div className="App">
-      <div className="main-part">
-    
-        <Navbar
-        logo={logo} loginStatus={loginStatus}/>
+    <Router>
+      <Switch>
 
-        <AppPageBody loginStatus={loginStatus}/>
-      </div>
+          <Route exact path="/">
+              <div className="App">
+                  <div className="main-part">
 
-      <div className="side-menu" id="side-menu">
-        This is the side menu
-        <LogoImage
-          logo={logo}
-          imageWidth="80%"/>
-        <LoginButton loginStatus={loginStatus}/>
-        <LogoutButton loginStatus={loginStatus}/>
+                      <Navbar
+                          logo={logo} loginStatus={loginStatus}/>
 
-      </div>
-    </div>
+                      <AppPageBody loginStatus={loginStatus}/>
+                  </div>
+
+                  <div className="side-menu" id="side-menu">
+                      This is the side menu
+                      <LogoImage
+                          logo={logo}
+                          imageWidth="80%"/>
+                      <LoginButton loginStatus={loginStatus} onClick={handleLoginButtonClick}/>
+                      <LogoutButton loginStatus={loginStatus}/>
+
+                  </div>
+              </div>
+          </Route>
+
+          <Route path="/login">
+              <LoginPage setLoginStatus={setLoginStatus}/>
+          </Route>
+
+      </Switch>
+    </Router>
+
+
   );
 }
 
