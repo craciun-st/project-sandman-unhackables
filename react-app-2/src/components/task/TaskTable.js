5392a15d94a9cd68040a4c531893c5e1bf436f7f
DanielCIstrate <daniel.istr8@gmail.com>
2021-07-20 14:33:00 +0300

    Add sort by category button
    for the moment it applies the sorting automatically when adding a
    new task after the first click

--- react-app-2/src/components/task/TaskTable.js
+++ react-app-2/src/components/task/TaskTable.js
@@ -4,8 +4,40 @@ import { colorClassForCategory } from '../../util/cssHelperFunctions'
 import bootstrap from "bootstrap/dist/css/bootstrap.min.css";
 
 export default class TaskTable extends Component {
+
+
+
+    getUniqueCategoryList(props) {
+        return [...new Set(props.taskList.map(task => task.category))];
+
+    }
+
+    // filterByCategory(event) {
+        
+    // }
+
+    compareAlphabetically(firstWord, secondWord) {
+        
+    }
+
+    sortByCategory(props) {
+        props.taskList.sort((a,b) => (a.category.localeCompare(b.category)))
+    }
+    
     render() {
+
+
         return (
+            <div>
+            <select>
+                <option defaultChecked value = "all">All</option>
+                {this.getUniqueCategoryList(this.props).map((category) => (
+                    <option value ={category}></option>
+                
+                    ))}
+            </select>
+            <button onClick={this.sortByCategory(this.props)}>Sort by category</button>
+
             <table className="table table-success table-striped TaskTable">
                 {this.props.taskList.length > 0 ?
                     (<thead>
@@ -52,6 +84,7 @@ export default class TaskTable extends Component {
                     ))}
                 </tbody>
             </table>
+            </div>
         )
     }
 }
